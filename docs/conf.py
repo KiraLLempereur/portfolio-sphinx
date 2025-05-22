dff29da2d4a36a7d7085754a03129813cbc196cd
Kira Lempereur <kira.lempereur@gmail.com>
2025-05-22 15:23:38 -0500

    Adding docs folder and source files

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Kira Lempereur: Portfolio'
+copyright = '2025, Kira Lempereur'
+author = 'Kira Lempereur'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'groundwork'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+html_favicon = '_static/favicon.png'
+html_logo = '_static/logo.png'
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Welcome to Kira Lempereur's Docs as Code Extravaganza!
+======================================================
+
+**Lumache** (/lu'make/) is a Python library for cooks and food lovers that
+creates recipes mixing random ingredients.  It pulls data from the `Open Food
+Facts database <https://world.openfoodfacts.org/>`_ and offers a *simple* and
+*intuitive* API.
+
+Check out the :doc:`usage` section for further information.
+
+.. note::
+
+   This project is under active development.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   usage
